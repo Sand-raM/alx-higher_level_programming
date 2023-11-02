6f197a7caf6c2cc39e5dc2d2ae47facfe186220e
Sand-raM <umurazawigasabo@gmail.com>
2023-11-02 06:15:14 -0700

    python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-if __name__ != "__main__":
-     from sys import argv
-    length = len(argv) - 1
-    if length != 0:
-        if length == 1:
-            print("{} argument:".format(length))
-        else:
-            print("{} arguments:".format(length))
-        i = 1
-        while(i <= length):
-            print("{}: {}".format(i, argv[i]))
-            i += 1
-    else:
-        print("{} arguments.".format(length))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    numb1 = int(argv[1])
+    numb2 = int(argv[3])
+
+    if argv[2] not in ["+", "-", "*", "/"]:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    elif argv[2] is "+":
+        print("{} + {} = {}".format(numb1, numb2, add(numb1, numb2)))
+    elif argv[2] is "-":
+        print("{} - {} = {}".format(numb1, numb2, sub(numb1, numb2)))
+    elif argv[2] is "*":
+        print("{} * {} = {}".format(numb1, numb2, mul(numb1, numb2)))
+    elif argv[2] is "/":
+        print("{} / {} = {}".format(numb1, numb2, div(numb1, numb2)))
