479840cbd8e02690df40c58e436c7cbdca4b5388
Sand-raM <umurazawigasabo@gmail.com>
2023-11-02 06:08:01 -0700

    python

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    length = len(argv) - 1
-    if length != 0:
-        if length == 1:
-            print("{} argument:".format(length))
-        else:
-            print("{} arguments:".format(length))
-        i = 1
-        while(i <= length):
-            print("{}: {}".format(i, argv[i]))
-            i += 1
-    else:
-        print("{} arguments.".format(length))
+from sys import argv
+add = 0
+for s in argv[1:]:
+    add += int(s)
+print("{:d}".format(add))
