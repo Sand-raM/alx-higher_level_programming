d13fab74a4f12343c63601d98e5f1304c2eddee5
Sand-raM <umurazawigasabo@gmail.com>
2023-11-02 09:33:56 -0700

    python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
-    argc = len(argv) - 1
-    if argc != 3:
-        print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    argc = len(argv)
+    if argc != 4:
+        print('Usage: {} <a> <operator> <b>'.format(argv[0]))
         exit(1)
-    a, operator, b = argv[1], argv[2], argv[3]
-    a, b = int(a), int(b)
-    if operator == '+':
-        func = add
-    elif operator == '-':
-        func = sub
-    elif operator == '*':
-        func = mul
-    elif operator == '/':
-        func = div
+    ops = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div
+    }
+    if argv[2] in ops:
+        num1 = int(argv[1])
+        num2 = int(argv[3])
+        op = ops[argv[2]]
+        result = op(num1, num2)
+        print('{:d} {:s} {:d} = {:d}'.format(num1, argv[2], num2, result))
     else:
-        print("Unknown operator. Available operators: +, -, *, and /")
+        print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
-
-    result = func(a, b)
-    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
+    exit(0)
