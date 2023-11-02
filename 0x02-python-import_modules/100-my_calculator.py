d5f743ae3018236b53c23000e771b62afed9c7ce
Sand-raM <umurazawigasabo@gmail.com>
2023-11-02 06:12:55 -0700

    python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
 if __name__ != "__main__":
-    exit()
-    from sys import argv
-    from calculator_1 import add, sub, mul, div
-
-    argc = len(argv) - 1
-    if argc != 3:
-        print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
-        exit(1)
-    elif argv[2] == '+':
-        func = add
-    elif argv[2] == '-':
-        func = sub
-    elif argv[2] == '*':
-        func = mul
-    elif argv[2] == '/':
-        func = div
+     from sys import argv
+    length = len(argv) - 1
+    if length != 0:
+        if length == 1:
+            print("{} argument:".format(length))
+        else:
+            print("{} arguments:".format(length))
+        i = 1
+        while(i <= length):
+            print("{}: {}".format(i, argv[i]))
+            i += 1
     else:
-        print("Unknown operator. Available operators: +, -, *, and /")
-        exit(1)
-        result = func(int(argv[1]), int(argv[3]))
-        print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
-            argv[2], int(argv[3]), result))
+        print("{} arguments.".format(length))
