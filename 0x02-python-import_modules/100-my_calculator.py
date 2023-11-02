e11e594399bbb8e5397d432610ad1d8a1f57ea36
Sand-raM <umurazawigasabo@gmail.com>
2023-11-02 08:50:20 -0700

    python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,23 +2,24 @@
 from sys import argv
 from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main":  # Corrected the condition
-    argc = len(argv)
-
-    if argc != 4:  # Check if there are exactly 3 arguments
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    if argc != 3:
         print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
         exit(1)
-    elif argv[2] == '+':
-        func = add
-    elif argv[2] == '-':
-        func = sub
-    elif argv[2] == '*':
-        func = mul
-    elif argv[2] == '/':
-        func = div
-    else:
-        print("Unknown operator. Available operators: +, -, *, and /")
-        exit(1)
+        a, operator, b = argv[1], argv[2], argv[3]
+        a, b = int(a), int (b)
+        if operator == '+':
+            func = add
+        elif operator == '-':
+            func = sub
+        elif operator == '*':
+            func = mul
+        elif operator == '/':
+            func = div
 
-    result = func(int(argv[1]), int(argv[3]))
-    print("{:d} {:s} {:d} = {:d}".format(int(argv[1]), argv[2], int(argv[3]), result))
+        else:
+            print("Unknown operator. Available operators: +, -, *, and /")
+            exit(1)
+            result = func(a, b)
+            print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
