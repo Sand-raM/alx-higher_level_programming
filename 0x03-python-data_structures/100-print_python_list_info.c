04594332d157209022ee989aff1ea93ed4f5a47c
Sand-raM <umurazawigasabo@gmail.com>
2023-11-06 07:34:01 -0800

    python

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,7 @@
-#include <python.h>
-#include <object.h>
-#include <listobject.h>
+#include "/usr/include/python3.4/Python.h"
+#include "/usr/include/python3.4/listobject.h"
+#include "/usr/include/python3.4/object.h"
+#include <stdio.h>
 
 void print_python_list_info(PyObject *p)
 {
