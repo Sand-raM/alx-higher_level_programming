0613c5c2bb6e1994693476e98b68d96fb2ea0a67
Sand-raM <umurazawigasabo@gmail.com>
2023-11-21 04:21:19 -0800

    python

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-"""defines aclass square"""
+"""Defines a class Square."""
 
 
-class square:
-    """represents a square"""
+class Square:
+    """Represents a square."""
 
     def __init__(self, size=0):
-        """initializes a new square.
+        """Initializes a new square.
 
         Args:
-        size (int): size of the new square.
+            size (int): The size of the new square.
         """
-        self.size = size
+        self.__size = size
 
-        @property
-        def size(self):
-            """"get/set the current size of the square"""
-            return (self.__size)
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
 
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an interger")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-            def area(self):
-                """return the current area of the square"""
-                return (self.__size ** 2)
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size ** 2)
